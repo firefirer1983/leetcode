bf826b65d8bcc347218f63d5038a830a5fe6b12e
xy <zhangxuyi@wanshare.com>
2019-12-20 21:18:45 +0800

    final answer

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -7,17 +7,20 @@ class Solution:
         if length == 1:
             return s
         longest = ""
-        print("check:", s)
 
         for pos in range(1, length):
-            # res = self.check_odd(pos, s)
-            # if len(res) > len(longest):
-            #     longest = str(res)
+            res = self.check_odd(pos, s)
+            
+            if len(res) > len(longest):
+                longest = str(res)
 
             res = self.check_even(pos, s)
             if len(res) > len(longest):
                 longest = str(res)
-
+                
+        if not longest and s:
+            return s[0]
+        
         return longest
 
     @staticmethod
@@ -45,17 +48,15 @@ class Solution:
         while True:
             if s[pos - offset - 1] != s[pos + offset]:
                 return s[pos - offset : pos + offset]
-
-            if (pos - offset - 1) < 0 or (pos + offset + 1) > len(s) - 1:
-                break
+            
+            if (pos - offset - 2) < 0 or (pos + offset + 1) > len(s) - 1:
+                return s[pos - offset -1: pos + offset + 1]
 
             offset += 1
-
-        return s[pos - offset - 1 : pos + offset + 1]
 
 
 if __name__ == "__main__":
     sol = Solution()
-    ts = "aaaa"
+    ts = "aaaaacc"
     ans = sol.longestPalindrome(ts)
     print("final:", ans)
