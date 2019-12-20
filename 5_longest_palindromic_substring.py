dab8ce6dbddf787715954c6ca9c3f04ceee03eb6
xy <zhangxuyi@wanshare.com>
2019-12-20 21:19:29 +0800

    final answer

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -10,17 +10,17 @@ class Solution:
 
         for pos in range(1, length):
             res = self.check_odd(pos, s)
-            
+
             if len(res) > len(longest):
                 longest = str(res)
 
             res = self.check_even(pos, s)
             if len(res) > len(longest):
                 longest = str(res)
-                
+
         if not longest and s:
             return s[0]
-        
+
         return longest
 
     @staticmethod
@@ -36,21 +36,20 @@ class Solution:
                 return s[pos - offset + 1 : pos + offset]
 
             if pos - offset - 1 < 0 or pos + offset + 1 > len(s) - 1:
-                break
+                return s[pos - offset : pos + offset + 1]
 
             offset += 1
-
-        return s[pos - offset : pos + offset + 1]
 
     @staticmethod
     def check_even(pos, s):
         offset = 0
         while True:
+            
             if s[pos - offset - 1] != s[pos + offset]:
                 return s[pos - offset : pos + offset]
-            
+
             if (pos - offset - 2) < 0 or (pos + offset + 1) > len(s) - 1:
-                return s[pos - offset -1: pos + offset + 1]
+                return s[pos - offset - 1 : pos + offset + 1]
 
             offset += 1
 
