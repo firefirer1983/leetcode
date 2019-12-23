a3e7ee4f4aa66c17c04b439caabd62bcf8496e83
xy <zhangxuyi@wanshare.com>
2019-12-23 10:44:23 +0800

    add config

---
+++ 12_integer_to_roman.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+class Solution:
+    def intToRoman(self, num):
+        roman = {
+            1: "I",
+            5: "V",
+            10: "X",
+            50: "L",
+            100: "C",
+            500: "D",
+            1000: "M",
+        }
+        weight = 1000
+        ret = ""
+        while num > 0:
+            unit = roman[weight]
+            r = num / weight
+            if r == 0:
+                pass
+            elif r <= 3:
+                ret += r * unit
+            elif r < 5:
+                ret += (5 - r) * unit + roman[weight * 5]
+            elif r <= 8:
+                ret += roman[weight * 5] + (r - 5) * unit
+            else:
+                ret += (10 - r) * unit + roman[weight * 10]
+            weight = weight / 10
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    tst = 113
+    ans = sol.intToRoman(tst)
+    print(ans)
